0af8144f32956d12e7b9a8a65a4228c60b9e1a40
Robert Gingras <developer@three-point-five.dev>
2024-01-21 10:04:33 -0500

    make the terminal normal sized on launch

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -20,13 +20,15 @@ config.font = wezterm.font {
   family = 'IosevkaTermNerdFontMono',
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 }
-config.default_prog = {'/usr/bin/bash'}
+config.default_prog = { '/usr/bin/bash' }
 config.color_scheme = color_scheme
 config.window_background_opacity = 0.9
 config.hide_tab_bar_if_only_one_tab = true
 config.pane_focus_follows_mouse = true
 config.use_fancy_tab_bar = false
 config.audible_bell = 'Disabled'
+config.initial_cols = 100
+config.initial_rows = 28
 config.colors = {
   tab_bar = {
     background = colors.background,
