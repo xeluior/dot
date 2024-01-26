1d7e55d5e7366c60b83eeafb0ebb72d0eecbea05
Robert Gingras <developer@three-point-five.dev>
2024-01-26 18:10:47 -0500

    further customized init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -150,12 +150,13 @@ require('lazy').setup({
   },
 
   {
-    -- Theme inspired by Atom
     'folke/tokyonight.nvim',
     priority = 1000,
     config = function()
       vim.cmd.colorscheme 'tokyonight-night'
-      vim.cmd.highlight 'Normal guibg=NONE ctermbg=NONE'
+      for _,mode in ipairs({'Normal', 'SignColumn', 'NormalNC'}) do
+        vim.cmd.highlight {args = { mode, 'guibg=NONE', 'ctermbg=NONE' } }
+      end
     end,
   },
 
@@ -166,7 +167,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'tokyonight',
         component_separators = '|',
         section_separators = '',
       },
@@ -215,19 +216,11 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    opts = {}
+  }
 }, {})
 
 -- [[ Setting options ]]
@@ -274,6 +267,18 @@ vim.o.termguicolors = true
 -- fix split direction
 vim.o.splitright = true
 vim.o.splitbottom = true
+
+-- words need to wrap correctly
+vim.o.linebreak = true
+
+-- two spaces gang
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+
+-- ask to save buffers on close
+vim.o.confirm = true
+
+vim.o.relativenumber = true
 
 -- [[ Basic Keymaps ]]
 
