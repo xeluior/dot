4daf1cf93b6d64713509ca88f74ef15059721b14
Robert Gingras <developer@three-point-five.dev>
2024-02-22 16:10:32 -0500

    improve the lvim config

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -2,3 +2,9 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+lvim.colorscheme = 'tokyonight-night'
+lvim.transparent_window = true
+lvim.builtin.terminal.direction = 'horizontal'
+lvim.builtin.terminal.size = function(_)
+  return vim.o.lines * 0.35
+end
