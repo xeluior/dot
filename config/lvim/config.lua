7c08e91c6f866a193caf1d89bf3bd17f05481658
Robert Gingras <developer@three-point-five.dev>
2024-02-22 16:27:32 -0500

    I thought this was LVIM default...

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -2,6 +2,7 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+vim.o.confirm = true
 lvim.colorscheme = 'tokyonight-night'
 lvim.transparent_window = true
 lvim.builtin.terminal.direction = 'horizontal'
