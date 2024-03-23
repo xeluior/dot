b9c6e154dbec699b84d308ca0478c0b13f085119
Robert Gingras <developer@three-point-five.dev>
2024-03-23 16:26:59 -0400

    hyprland ricing start

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local config = {}
 config.color_scheme = 'tokyonight_night'
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.95
+config.hide_mouse_cursor_when_typing = true
 config.font = wezterm.font {
   family = 'IosevkaTermNerdFontMono',
   harfbuzz_features = { 'calt=0' }
