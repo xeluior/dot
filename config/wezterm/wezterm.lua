430a2310633943945f6bc3fea26cfe2f06f10b0d
Robert Gingras <developer@three-point-five.dev>
2024-03-23 11:25:42 -0400

    tmux isn't great but I need it for some lowend systems

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'tokyonight_night'
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.95
+config.font = wezterm.font {
+  family = 'IosevkaTermNerdFontMono',
+  harfbuzz_features = { 'calt=0' }
+}
+
+return config
