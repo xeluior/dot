d58f511a4aa93be785280ca98c2b834be94b5177
Robert Gingras <developer@three-point-five.dev>
2024-04-06 17:50:22 -0400

    inits modbash with my dotfiles

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,10 +7,14 @@ function setup() {
   ln -si "$src" "$dst"
 }
 
+modbash_init () {
+  # use a subshell with my XDG_* vars defined
+  BASH_ENV="$PWD/profile" bash -c 'git clone https://github.com/xeluior/modbash.git "$XDG_DATA_HOME/modbash"'
+}
+
 function main() {
   setup "${PWD}/profile" "${HOME}/.profile"
   setup "${PWD}/bashrc" "${HOME}/.bashrc"
-  setup "${PWD}/bash_profile" "${HOME}/.bash_profile"
   for f in "${PWD}/config/"*; do
     setup "$f" "${XDG_CONFIG_HOME:-"${HOME}/.config"}"
   done
